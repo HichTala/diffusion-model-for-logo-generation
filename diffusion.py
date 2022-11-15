088ebe556c24283a88eea0179300261eef2763f7
Antoine <antoine.bruez@mines-ales.org>
2022-11-15 12:33:57 +0000

    Initializes export folder

--- diffusion.py
+++ diffusion.py
@@ -11,4 +11,4 @@ pipe = pipe.to(device)
 prompt = "a photo of an astronaut riding a horse on mars"
 image = pipe(prompt).images[0]  
     
-image.save("astronaut_rides_horse.png")
+image.save("img/astronaut_rides_horse.png")
